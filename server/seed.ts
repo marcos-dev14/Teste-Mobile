dcd9d0892df7e5b47dd1b1e68aa05a80423f746d
marcos-dev14 <mprogramador.10@gmail.com>
2025-02-28 00:32:30 -0300

    fix: ajuste no arquivo do seed - Backend

--- server/seed.ts
+++ server/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
@@ -28,7 +29,7 @@ async function main() {
       name: 'Calça Jeans Skinny',
       description: 'Calça jeans skinny feminina, ajustada e confortável.',
       price: 89.99,
-      rating: 3.5,
+      rating: 1,
       images: [
         'https://images.unsplash.com/photo-1542272604-787c3835535d?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
         'https://images.unsplash.com/photo-1604176354204-9268737828e4?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
@@ -38,7 +39,7 @@ async function main() {
       name: 'Bolsa de Couro',
       description: 'Bolsa de couro sintético, elegante e durável.',
       price: 99.99,
-      rating: 3.5,
+      rating: 5,
       images: [
         'https://images.unsplash.com/photo-1566150902887-9679ecc155ba?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
       ],
@@ -56,7 +57,7 @@ async function main() {
       name: 'Calça Jogger',
       description: 'Calça jogger masculina, moderna e versátil.',
       price: 79.99,
-      rating: 3.5,
+      rating: 4,
       images: [
         'https://images.unsplash.com/photo-1602293589930-45aad59ba3ab?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
       ],
@@ -65,7 +66,7 @@ async function main() {
       name: 'Bolsa Transversal',
       description: 'Bolsa transversal pequena, ideal para o dia a dia.',
       price: 69.99,
-      rating: 3.5,
+      rating: 2,
       images: [
         'https://images.unsplash.com/photo-1594035910387-fea47794261f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
       ],
@@ -80,6 +81,34 @@ async function main() {
   }
 
   console.log('Seed concluído: Produtos criados com sucesso!');
+
+  // Criar o usuário
+  const userEmail = 'bob@example.com';
+  const userPassword = '10203040';
+
+  // Verifica se o usuário já existe
+  const existingUser = await prisma.user.findUnique({
+    where: { email: userEmail },
+  });
+
+  if (existingUser) {
+    console.log('Usuário já existe:', userEmail);
+  } else {
+    // Criptografa a senha
+    const saltRounds = 12;
+    const hashedPassword = await bcrypt.hash(userPassword, saltRounds);
+
+    // Cria o usuário
+    await prisma.user.create({
+      data: {
+        email: userEmail,
+        password: hashedPassword,
+        name: 'Bob', // Nome opcional
+      },
+    });
+
+    console.log('Usuário criado com sucesso:', userEmail);
+  }
 }
 
 main()
